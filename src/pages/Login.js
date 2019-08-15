c5d0ec5c565ca63acb567a268c8cf40b880ebb66
Samantha Silva <samanthasilva.n@gmail.com>
2019-08-15 18:42:57 -0300

    Photos.

---
+++ src/pages/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+export default function Login() {
+    return ( 
+        <View style={styles.container}>
+            
+        </View>
+        );
+}
+
+
+const styles = StyleSheet.create({
+    container: {}
+})
